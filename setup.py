c281072657428d27b8b72fd90e6c6fa486bea57d
brianmanderson <markba122@gmail.com>
2024-08-11 13:41:43 -0400

    needed to add a .

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setup(
     name='PlotScrollNumpyArrays',
     author='Brian Mark Anderson',
     author_email='markba122@gmail.com',
-    version='2.0.1',
+    version='2.0.2',
     description='Services for plotting and viewing',
     long_description=long_description,
     long_description_content_type="text/markdown",
--- src/PlotScrollNumpyArrays/Plot_Scroll_Images.py
+++ src/PlotScrollNumpyArrays/Plot_Scroll_Images.py
@@ -1,4 +1,4 @@
-from PlotScroll import *
+from .PlotScroll import *
 
 
 if __name__ == '__main__':
