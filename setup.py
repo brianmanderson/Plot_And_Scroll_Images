d20225210925fc5e4d2e5222db4f1f93ef57581f
brianmanderson <markba122@gmail.com>
2024-08-11 13:29:59 -0400

    updates

---
+++ setup.py
@@ -0,0 +1,29 @@
+__author__ = 'Brian M Anderson'
+# Created on 9/15/2020
+
+
+from setuptools import setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+with open('requirements.txt') as f:
+    required = f.read().splitlines()
+
+setup(
+    name='DicomRTTool',
+    author='Brian Mark Anderson',
+    author_email='markba122@gmail.com',
+    version='2.0.0',
+    description='Services for plotting and viewing',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    package_dir={'Plot_Scroll_Images': 'src/Plot_Scroll_Images'},
+    packages=['Plot_Scroll_Images'],
+    include_package_data=True,
+    url='https://github.com/brianmanderson/Dicom_RT_and_Images_to_Mask',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU Affero General Public License v3",
+    ],
+    install_requires=required,
+)
---
+++ src/Plot_Scroll_Images/__init__.py
@@ -0,0 +1,5 @@
+from .PlotScroll import *
+
+
+if __name__ == '__main__':
+    pass
