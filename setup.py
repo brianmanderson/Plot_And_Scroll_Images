11d55001930c21b40342ecc883caaeaf8809f777
Brian Anderson <markba122@gmail.com>
2024-08-16 16:27:42 -0400

    attempt to push

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setup(
     name='PlotScrollNumpyArrays',
     author='Brian Mark Anderson',
     author_email='markba122@gmail.com',
-    version='2.0.3',
+    version='2.0.4',
     description='Services for plotting and viewing',
     long_description=long_description,
     long_description_content_type="text/markdown",
